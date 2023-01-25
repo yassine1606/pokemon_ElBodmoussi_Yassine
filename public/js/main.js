b3e31763b177b9f228907d5c95471a65a71c4497
Yassine Ebd <elbodmoussiyassine@gmail.com>
2023-01-25 16:53:03 +0100

    Avancement

--- public/js/main.js
+++ public/js/main.js
@@ -5,30 +5,57 @@ let atk3 = document.getElementById('atk3');
 let atk4 = document.getElementById('atk4');
 
 
+function playMusic() {
+    let music = document.getElementById('background-music');
+    music.play();
+}
 
 
+let btgo2 = document.getElementById('btgo2')
+btgo2.addEventListener('click',()=>{
+    dtotal.setAttribute('class','revenir')
+    playMusic()
+
+})
+let arca = document.getElementById('arca');
+let btgo = document.getElementById('btgo')
+let dtotal = document.getElementById('dtotal')
+btgo.addEventListener('click',()=>{
+    arca.setAttribute('src', './public/img/arcanin.png');
+    dtotal.setAttribute('class','revenir');
+    playMusic();
+})
+
+
+
+let clickAtk = new Audio();
+clickAtk.src = "./public/img/DoubleKickSingle.wav";
 
 let action = document.getElementById('action')
 
 atk1.addEventListener('click', () => {
-    action.innerHTML = 'Arcanin lance Boute feu : Léviator perd 20 HP ! <br> <br>  Léviator lance Ouragan : Arcanin perd 40 HP !'
+    clickAtk.play();
+    action.innerHTML = 'Arcanin lance Boute feu : Léviator perd 40 HP !'
     let hpEnnemy = document.getElementById("hpEnnemy").textContent;
     hpEnnemy -= 20;
+    
     document.getElementById("hpEnnemy").textContent = hpEnnemy;
     let barreEnnemy = document.getElementById("hpBar").value;
     barreEnnemy -= 30;
     document.getElementById("hpBar").value = barreEnnemy;
     if (hpEnnemy <= 0) {
         alert('Léviator est mort !')
-
     } else {
-        leviatorAttack(40);
+        setTimeout(() =>{
+            leviatorAttack(40)
+        }, 2000);
     }
 })
 
 
 atk2.addEventListener('click', () => {
-    action.innerHTML = 'Arcanin lance Crocs Feu : Léviator perd 30 HP  ! <br> <br>  Léviator lance Siphon : Arcanin perd 20 HP !'
+    clickAtk.play();
+    action.innerHTML = 'Arcanin lance Crocs Feu : Léviator perd 20 HP !'
     hpEnnemy = document.getElementById("hpEnnemy").textContent;
     hpEnnemy -= 30;
     document.getElementById("hpEnnemy").textContent = hpEnnemy;
@@ -39,12 +66,15 @@ atk2.addEventListener('click', () => {
         alert('Léviator est mort !')
 
     } else {
-        leviatorAttack(20);
+        setTimeout(() =>{
+            leviatorAttack2(20)
+        }, 2000);
     }
 })
-
 atk3.addEventListener('click', () => {
-    action.innerHTML = 'Arcanin lance Rouee Feu ! <br> <br>  Léviator lance Cascade !'
+    clickAtk.play();
+
+    action.innerHTML = 'Arcanin lance Rouee Feu !'
     hpEnnemy = document.getElementById("hpEnnemy").textContent;
     hpEnnemy -= 20;
     document.getElementById("hpEnnemy").textContent = hpEnnemy;
@@ -55,25 +85,89 @@ atk3.addEventListener('click', () => {
         alert('Léviator est mort !')
 
     } else {
-        leviatorAttack(20);
+        setTimeout(() =>{
+            leviatorAttack3(50)
+        }, 2000);
     }
 })
 
 atk4.addEventListener('click', () => {
+    clickAtk.play();
     action.innerHTML = 'Arcanin lance Hurlement !'
+    hpEnnemy = document.getElementById("hpEnnemy").textContent;
+    hpEnnemy -= 20;
+    document.getElementById("hpEnnemy").textContent = hpEnnemy;
+    barreEnnemy = document.getElementById("hpBar").value;
+    barreEnnemy -= 30;
+    document.getElementById("hpBar").value = barreEnnemy;
+    if (hpEnnemy <= 0) {
+        alert('Léviator est mort !')
+
+    } else {
+        setTimeout(() =>{
+            leviatorAttack4(12)
+        }, 2000);
+    }
 })
 
 
-
+let clickSound = new Audio();
+clickSound.src = "./public/img/atklevi.mp3";
 
 function leviatorAttack(damage) {
-    let hpAlly = document.getElementById('pvAlly');
+    clickSound.play();
+    action.innerHTML = 'Léviator lance Siphon : Arcanin perd 40 HP !'
+    let hpAlly = document.getElementById('pvAlly').textContent;
     hpAlly -= damage;
-    document.getElementById('pvAlly').value = hpAlly;
+    document.getElementById('pvAlly').textContent = hpAlly;
     let barreAlly = document.getElementById("hpBarAlly").value;
+
     barreAlly -= damage;
     document.getElementById("hpBarAlly").value = barreAlly;
-    if (barreAlly <= 0) {
-        alert("Game Over ! Arcanin est mort");
+    if (hpAlly <= 0) {
+        alert("Game Over ! Léviator est mort");
     }
 }
+function leviatorAttack2(damage) {
+    clickSound.play();
+    action.innerHTML = 'Léviator lance Hydrocanon : Arcanin perd 20 HP !'
+    let hpAlly = document.getElementById('pvAlly').textContent;
+    hpAlly -= damage;
+    document.getElementById('pvAlly').textContent = hpAlly;
+    let barreAlly = document.getElementById("hpBarAlly").value;
+
+    barreAlly -= damage;
+    document.getElementById("hpBarAlly").value = barreAlly;
+    if (hpAlly <= 0) {
+        alert("Game Over ! Léviator est mort");
+    }
+}
+function leviatorAttack3(damage) {
+    clickSound.play();
+    action.innerHTML = 'Léviator lance Surf : Arcanin perd 50 HP !'
+    let hpAlly = document.getElementById('pvAlly').textContent;
+    hpAlly -= damage;
+    document.getElementById('pvAlly').textContent = hpAlly;
+    let barreAlly = document.getElementById("hpBarAlly").value;
+
+    barreAlly -= damage;
+    document.getElementById("hpBarAlly").value = barreAlly;
+    if (hpAlly <= 0) {
+        alert("Game Over ! Léviator est mort");
+    }
+}
+function leviatorAttack4(damage) {
+    clickSound.play();
+    action.innerHTML = 'Léviator lance Cascade : Arcanin perd 12 HP !'
+    let hpAlly = document.getElementById('pvAlly').textContent;
+    hpAlly -= damage;
+    document.getElementById('pvAlly').textContent = hpAlly;
+    let barreAlly = document.getElementById("hpBarAlly").value;
+
+    barreAlly -= damage;
+    document.getElementById("hpBarAlly").value = barreAlly;
+    if (hpAlly <= 0) {
+        alert("Game Over ! Léviator est mort");
+    }
+}
+
